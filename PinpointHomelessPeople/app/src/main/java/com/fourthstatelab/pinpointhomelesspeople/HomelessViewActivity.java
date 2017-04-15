62aae4c6ab180cbad9268d2d62ef4ec7b4e83326
sid25081998 <sridharswain25@hotmail.com>
2017-04-16 04:33:13 +0530

    Logo Changed

--- PinpointHomelessPeople/app/src/main/java/com/fourthstatelab/pinpointhomelesspeople/HomelessViewActivity.java
+++ PinpointHomelessPeople/app/src/main/java/com/fourthstatelab/pinpointhomelesspeople/HomelessViewActivity.java
@@ -1,23 +1,41 @@
 package com.fourthstatelab.pinpointhomelesspeople;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.media.Image;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import static com.fourthstatelab.pinpointhomelesspeople.Utility.nunito_bold;
 import static com.fourthstatelab.pinpointhomelesspeople.Utility.nunito_reg;
 
-public class HomelessViewActivity extends AppCompatActivity {
+public class HomelessViewActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
+    int index;
+    Homeless curr_homeless;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +46,9 @@ public class HomelessViewActivity extends AppCompatActivity {
         TextView sex=(TextView)findViewById(R.id.sexofhomeless);
         TextView other=(TextView)findViewById(R.id.otherdetailshomeless);
         Intent prev_intent=getIntent();
-        int i=prev_intent.getIntExtra("index",0);
+        index=prev_intent.getIntExtra("index",0);
 
-        Homeless curr_homeless=Data_holder.Homeless_list.get(i);
+        curr_homeless=Data_holder.Homeless_list.get(index);
         name.setText(curr_homeless.name+"");
         name.setTypeface(nunito_bold);
         age.setTypeface(nunito_reg);
@@ -50,5 +68,37 @@ public class HomelessViewActivity extends AppCompatActivity {
         {
 
         }
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        int permissionCheck = ContextCompat.checkSelfPermission(HomelessViewActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (ContextCompat.checkSelfPermission(HomelessViewActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(HomelessViewActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    1123);
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+
+        LatLng latlong=new LatLng(curr_homeless.loc_data.latitude,curr_homeless.loc_data.longitude);
+        Marker mymarker=mMap.addMarker(new MarkerOptions().position(latlong).title(curr_homeless.name));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlong, 15));
+        mymarker.showInfoWindow();
+        mymarker.setTag(index);
+    }
+
 }
